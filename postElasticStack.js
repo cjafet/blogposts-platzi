ab59aafc416c9da4d782169d7629e34583b04c27
Carlos Jafet Neto <cjafet07@gmail.com>
2019-03-10 22:50:19 -0300

    Update postElasticStack.js
    Remove comentários e códigos desnecessários.

--- postElasticStack.js
+++ postElasticStack.js
@@ -1,8 +1,6 @@
 const AWS = require('aws-sdk');
 var s3 = new AWS.S3();
 const fetch = require('node-fetch');
-let count = 0;
-var today = new Date();
 
 var params = {
     Bucket: "cjafet/platzi", 
@@ -13,7 +11,6 @@ var params = {
     if (err) {
         console.log(err, err.stack); // an error occurred
     } else {
-        //console.log(data.Body.toString('utf-8'));
         obj = JSON.parse(data.Body.toString('utf-8'));
         console.log(obj);
         
@@ -24,12 +21,7 @@ var params = {
             console.log('end of call');
         }
 
-
-        // TO POST DATA TO A ES DB WE NEED TO CONSTRCUT OUR URL IN THE FOLLOWING FORMAT
-        // url = host/index/type
         function callApi(product) {
-            console.log(product);
-            // JSON.stringify(product)
             product_obj = JSON.stringify(product);
             console.log(product_obj);
             return fetch('https://endpoint.region.es.amazonaws.com:443/nameof-your-index/doc-type', { method: 'POST', body: product_obj, headers: { 'Content-Type': 'application/json' } })
